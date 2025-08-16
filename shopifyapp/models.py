e2131f23bca9973f22ad8668808b29d32754dc80
Om Jagtap <omjagtap678@gmail.com>
2025-08-16 12:22:09 +0530

    First commit - SkillsShopify Django Project

---
+++ shopifyapp/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    discription = models.TextField()
+    original_price = models.FloatField()
+    discount_percentage = models.IntegerField()
+    selling_price = models.FloatField()
+    image = models.ImageField(upload_to='media')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
+
+    def __str__(self):
+        return self.name
